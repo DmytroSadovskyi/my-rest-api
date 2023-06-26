20e6b596d8955b828c001d85e7cc0ecb534be0a6
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-06-26 10:46:18 +0300

    change register function

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,9 @@
 const bcryptjs = require("bcryptjs");
 const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,7 +15,15 @@ const register = async (req, res) => {
   const hashPassword = await bcryptjs.hash(password, 10);
   const newUser = await User.create({ ...req.body, password: hashPassword });
 
+  const payload = {
+    id: newUser._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "7d" });
+  await User.findByIdAndUpdate(newUser._id, { token });
+
   res.status(201).json({
+    token,
     name: newUser.name,
     email: newUser.email,
   });
